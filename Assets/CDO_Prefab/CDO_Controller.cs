23c3de036920ab4d8b2d87b5d0dd311471799c12
CHM <190792910+Tajo-Nero@users.noreply.github.com>
2025-04-16 11:50:22 +0900

    [feat] 이시온 브런치에서 내껄로 머지함
    머지후 프리팹없는거 다시 지우고 넣음

--- Assets/CDO_Prefab/CDO_Controller.cs
+++ Assets/CDO_Prefab/CDO_Controller.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class CDO_Controller : MonoBehaviour
 {
-    public CDO_Soldier testScript; // Test 스크립트 참조
+    public CDO_Soldier CDO_SoldierCtrScript; // Test 스크립트 참조
 
     void Start()
     {
-        if (testScript == null)
+        if (CDO_SoldierCtrScript == null)
         {
             Debug.LogError("스크립트 꽂아라");
         }
@@ -14,6 +14,6 @@ public class CDO_Controller : MonoBehaviour
 
     void Update()
     {
-       testScript.HandleKeyInput();
+       CDO_SoldierCtrScript.HandleKeyInput();
     }
 }
