f41c08849f80454cc34a63775b1389aaa11765d7
ZION <warmth.giver@gmail.com>
2025-04-30 17:38:34 +0900

    모든 작업 머지

--- Assets/Workspace/YJH/Scripts/Trigger.cs
+++ Assets/Workspace/YJH/Scripts/Trigger.cs
@@ -1,12 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 using UnityEngine.Events;
-using ZL.Unity.Vigil;
 
 public class Trigger : MonoBehaviour
 {
-    public UnityEvent onCollisionEnterEvent;
+    [Space]
+
+    [SerializeField]
+
+    private UnityEvent onCollisionEnterEvent;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
